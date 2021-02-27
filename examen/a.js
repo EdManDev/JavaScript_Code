f4d6660698ea6abc829adc9c1fc789b322ed1f0f
EdManDev <ed.manigat@gmail.com>
2021-02-26 20:07:51 -0400

    update some shit

---
+++ examen/a.js
@@ -0,0 +1,51 @@
+// (let = 23),
+// 	setTimeout(function () {
+// 		modal.classList.remove("hidden");
+// 	}, 10000);
+// console.log("Results shown");
+// ------------------------------------------------
+
+// let discountPrice function(price) {
+// return price * 0.85;
+// };
+// ------------------------------------------------
+
+// class TaxCalculator {
+// 	static calculate(total) {
+// 		return total * 0.05;
+// 	}
+// }
+
+// console.log(TaxCalculator);
+// ------------------------------------------------
+
+// "use strict";
+// function logThis() {
+// this.desc "logger";
+// console.log(this);
+// }
+// new logThis();
+
+// ------------------------------------------------
+// console.log(typeof 42);
+// ------------------------------------------------
+
+// const x = 6 % 2;
+// const y = x ? "One" : "Two";
+// console.log(y);
+
+// ------------------------------------------------
+
+// Student.prototype = new Person();
+// Student.prototype Person;
+// Student.prototype Person();
+// Student.parent = Person;
+
+// ------------------------------------------------
+// let roadTypes = ["street", "road", "avenue", "circle"];
+// console.log(roadTypes);
+// ------------------------------------------------
+
+// let a = 5;
+// console.log(++a);
+// ------------------------------------------------
