e7923059c99f940d3e2e8e8d0be4d5552a3c1bf5
EdManDev <ed.manigat@gmail.com>
2021-09-16 11:44:29 -0400

    update

--- condition.js
+++ condition.js
@@ -1,19 +1,25 @@
 function myFunction() {
-	var text;
-	var fruits = document.getElementById("myInput").value;
+	const text = null;
+	const fruits = "Orange";
+	// var fruits = document.getElementById("myInput").value;
 
 	switch (fruits) {
 		case "Banana":
-			text = "Banana is good!";
+			// text = "Banana is good!";
+			console.log("Banana is good!");
 			break;
 		case "Orange":
-			text = "I am not a fan of orange.";
+			// text = "I am not a fan of orange.";
+			console.log("Orange is good!");
 			break;
 		case "Apple":
-			text = "How you like them apples?";
+			// text = "How you like them apples?";
+			console.log("Apple is good!");
 			break;
 		default:
-			text = "I have never heard of that fruit...";
+			// text = "I have never heard of that fruit...";
+			console.log("I have never heard of that fruit...");
 	}
-	document.getElementById("demo").innerHTML = text;
+	// document.getElementById("demo").innerHTML = text;
 }
+console.log(myFunction());
