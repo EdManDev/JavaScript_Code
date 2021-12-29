ebcfb6ae1aa98ae9f326a656ef447ef4eb7b5b55
EdManDev <ed.manigat@gmail.com>
2021-12-29 15:42:31 -0400

    node mailer

---
+++ node.js/Nodemailer.js
@@ -0,0 +1,31 @@
+// ------------------------------------------------------------MY NodeMailer TEST But Working
+// const transporter = nodemailer.createTransport({
+// 	service: "gmail",
+// 	host: process.env.SMTP_HOST,
+// 	port: process.env.SMTP_PORT,
+// 	auth: {
+// 		user: process.env.SMTP_EMAIL,
+// 		pass: process.env.SMTP_PASSWORD,
+// 	},
+// });
+
+// // const email = "ed.manigat@gmail.com";
+
+// // send mail with defined transport object
+// const info = {
+// 	from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`, // sender address
+// 	to: email, // list of receivers
+// 	subject: "Hello ✔", // Subject line
+// 	text: "Hello world?", // plain text body
+// 	html: welcome(), // html body
+// };
+
+// transporter.sendMail(info, (err, info) => {
+// 	if (err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log(info);
+// 	}
+// 	transporter.close();
+// });
+// ------------------------------------------------------------MY NodeMailer TEST But Working
