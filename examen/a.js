c4a25a82f0a53bad9f4a857420e6d0968cfde009
EdManDev <ed.manigat@gmail.com>
2022-03-29 05:34:16 -0400

    update

--- examen/a.js
+++ examen/a.js
@@ -132,6 +132,21 @@
 // });
 
 // ------------------------------------
+//  TRURING
+function find_max(nums) {
+	let max_num = Number.NEGATIVE_INFINITY; // smaller than all other numbers
+
+	for (let num of nums) {
+		if (num > max_num) {
+			// (Fill in the missing line here)
+			num = max_num;
+		}
+	}
+	return max_num;
+	// console.log(max_num);
+}
+console.log(find_max);
+
 // ------------------------------------
 // ------------------------------------
 // ------------------------------------
---
+++ node.js/json/example1.js
@@ -0,0 +1,6 @@
+var json =
+	'[{"user": "001", "text": "Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit."},{"user": "002", "text": "Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit."},{"user": "003", "text": "Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit."},     {"user": "004", "text": "Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit."},      {"user": "005", "text": "Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit."}],     {"user": "006", "text": "Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit."}]';
+
+// alert(JSON.stringify(json));
+alert(json);
+console.log(json);
