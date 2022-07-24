b6d16a52f7e5bdbb6f7d3ca4479340b4555e811a
EdManDev <ed.manigat@gmail.com>
2022-07-24 00:24:32 -0400

    concatenation of two number string

---
+++ concatenation.js
@@ -0,0 +1,12 @@
+// var str = "Name";
+// var num = 7;
+// var joint = str + num;
+
+// console.log(joint);
+
+// concatenation of two number string
+var strNum1 = "09";
+var strNum2 = "80";
+var joint = strNum1 + strNum2;
+
+console.log(joint);
