8e13b0d5ed27daa3a372ae4702168e24f70c01a4
EdManDev <ed.manigat@gmail.com>
2022-11-02 20:54:41 -0400

    ok

--- ID-generate.js
+++ ID-generate.js
@@ -1,5 +1,9 @@
-var msg = 'Name Surname';
-var acronym = msg.match(/\b(\w)/g).join('');
-var number = Math.floor(10000 + Math.random() * 90000);
+// var msg = 'Name Surname ll';
+// var msgUP = msg.toUpperCase(); // upercase mu string
+// var acronym = msgUP.match(/\b(\w)/g).join(''); // give all first character of each word
+// var acronym2 = acronym.slice(0, 2); // giv only 2 leter
+// var number = Math.floor(10000 + Math.random() * 90000); // generate random number
+// const user_id = acronym2 + '-' + number;
+// console.log(user_id);
 
-console.log(acronym + '-' + number);
+// console.log(acronym + '-' + number);
